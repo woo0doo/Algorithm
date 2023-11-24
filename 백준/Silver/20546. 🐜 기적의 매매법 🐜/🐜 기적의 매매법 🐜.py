dcd2c2b56b3527f4742f8add268e2f55ce9be5cb
우영두 <duden5000@gmail.com>
2023-11-24 12:33:43 +0900

    [Silver V] Title: 🐜 기적의 매매법 🐜, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.py
+++ 백준/Silver/20546. 🐜 기적의 매매법 🐜/🐜 기적의 매매법 🐜.py
@@ -1,37 +1,48 @@
-cash=int(input())
+import sys
+input = sys.stdin.readline
 
-arr=list(map(int,input().split()))
-junMoney=cash
-junCnt=0
-sungMoney=cash
-sungCnt=0
-for i in arr:
-    junCnt += junMoney//i
-    junMoney %= i
+n = int(input())
 
-plusCnt=0
-minusCnt=0
-for i in range(1,len(arr)):
-    if arr[i] > arr[i-1]:
-        minusCnt=0
-        plusCnt+=1
-    elif arr[i] < arr[i-1]:
-        plusCnt=0
-        minusCnt+=1
+arr = list(map(int, input().split()))
 
-    if plusCnt >= 3:
-        sungMoney+=arr[i]*sungCnt
-        sungCnt=0
-    elif minusCnt >= 3:
-        sungCnt+=sungMoney//arr[i]
-        sungMoney%=arr[i]
+bnpMoney = n
+bnpStock = 0
+timingMoney = n
+timingStock = 0
+timingImproveCnt = 0
+timingDecreaseCnt = 0
+for i in range(len(arr)):
+    bnpStock += bnpMoney // arr[i]
+    bnpMoney = bnpMoney % arr[i]
 
-j = junMoney + junCnt * arr[-1]
-s = sungMoney + sungCnt * arr[-1]
+    if i == 0:
+        continue
+    else:
+        if arr[i] > arr[i-1]:
+            timingImproveCnt += 1
+            timingDecreaseCnt = 0
+        elif arr[i] == arr[i-1]:
+            timingImproveCnt = 0
+            timingDecreaseCnt = 0
+        else:
+            timingImproveCnt = 0
+            timingDecreaseCnt += 1
 
-if j>s:
+        if timingImproveCnt >= 3:
+            timingMoney += timingStock * arr[i]
+            timingStock = 0
+
+        if timingDecreaseCnt >= 3:
+            timingStock += timingMoney // arr[i]
+            timingMoney = timingMoney % arr[i]
+
+
+bnpTotalMoney = bnpMoney + arr[-1] * bnpStock
+timingTotalMoney = timingMoney + arr[-1] * timingStock
+
+if bnpTotalMoney > timingTotalMoney:
     print("BNP")
-elif j<s:
-    print("TIMING")
-else:
+elif bnpTotalMoney == timingTotalMoney:
     print("SAMESAME")
+else:
+    print("TIMING")
