b0acc573ac516fd792b8dcc459b51b1f401f06e5
우영두 <duden5000@gmail.com>
2023-12-01 13:53:22 +0900

    [Silver IV] Title: 알바생 강호, Time: 3940 ms, Memory: 34972 KB -BaekjoonHub

---
+++ 백준/Silver/1758. 알바생 강호/알바생 강호.py
@@ -0,0 +1,16 @@
+n = int(input())
+
+arr = []
+money = 0
+for _ in range(n):
+    tip = int(input())
+    arr.append(tip)
+
+arr.sort(reverse=True)
+
+for j in range(len(arr)):
+    if arr[j] - j <= 0:
+        break
+    money += arr[j] - j
+
+print(money)
