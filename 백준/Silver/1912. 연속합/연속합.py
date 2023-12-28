1e7cc0a9f1416d015b38b6a8a2774809ddc8024b
우영두 <duden5000@gmail.com>
2023-12-28 17:56:13 +0900

    [Silver II] Title: 연속합, Time: 96 ms, Memory: 38828 KB -BaekjoonHub

---
+++ 백준/Silver/1912. 연속합/연속합.py
@@ -0,0 +1,15 @@
+import sys
+input = sys.stdin.readline
+
+n = int(input())
+
+arr = list(map(int,input().split()))
+
+dp = [-1000] * (n+1)
+
+for i in range(len(arr)):
+    if i == 0:
+        dp[i] = arr[i]
+    else:
+        dp[i] = max(arr[i], dp[i-1]+arr[i])
+print(max(dp))
