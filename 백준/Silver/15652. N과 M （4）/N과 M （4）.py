ad707b02a965dd85eb933106158d8b6073534e72
우영두 <duden5000@gmail.com>
2024-01-11 13:04:15 +0900

    [Silver III] Title: N과 M (4), Time: 56 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,13 @@
+n, m = map(int,input().split())
+arr=[]
+def dfs(start):
+    if len(arr) == m:
+        print(*arr)
+        return
+
+    for i in range(start, n+1):
+        arr.append(i)
+        dfs(i)
+        arr.pop()
+
+dfs(1)
