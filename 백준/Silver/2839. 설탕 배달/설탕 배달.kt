bfa98f212084c3ebb0f7ab35f85e7cfe1a113c5c
우영두 <duden5000@gmail.com>
2024-03-13 19:04:24 +0900

    [Silver IV] Title: 설탕 배달, Time: 88 ms, Memory: 12344 KB -BaekjoonHub

---
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.kt
@@ -0,0 +1,23 @@
+fun main() {
+
+    var n = readLine()!!.toInt()
+
+    var cnt = 0
+
+    while (n >= 0) {
+        if (n%5 == 0) {
+            cnt += n/5
+            break
+        } else {
+            n -= 3
+            cnt += 1
+        }
+    }
+
+    if (n < 0) {
+        println(-1)
+    } else {
+        println(cnt)
+    }
+
+}
