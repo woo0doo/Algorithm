8d90b031c742d3a0f140db54bf6a1a852a926385
우영두 <duden5000@gmail.com>
2024-03-13 20:07:23 +0900

    [Silver IV] Title: 스택 2, Time: 1616 ms, Memory: 348348 KB -BaekjoonHub

--- 백준/Silver/28278. 스택 2/스택 2.kt
+++ 백준/Silver/28278. 스택 2/스택 2.kt
@@ -1,48 +1,26 @@
-import java.io.BufferedReader
-import java.io.InputStreamReader
 import java.lang.StringBuilder
 import java.util.Stack
 
-fun main() {
+fun main() = with(System.`in`.bufferedReader()) {
 
-    val br = BufferedReader(InputStreamReader(System.`in`))
     val bw = System.out.bufferedWriter()
     val sb = StringBuilder()
 
-    val n = br.readLine().toInt()
+    val n = readLine().toInt()
     val stack = Stack<Int>()
 
     repeat(n) {
 
-        val split = br.readLine().split(" ")
+        val split = readLine().split(" ")
 
         when (split[0]) {
             "1" -> stack.add(split[1].toInt())
-            "2" -> {
-                if (stack.empty()) {
-                    sb.append("${-1}\n")
-                } else {
-                    sb.append("${stack.pop()}\n")
-                }
-            }
+            "2" -> sb.append(if(stack.empty()) "${-1}\n" else "${stack.pop()}\n")
             "3" -> sb.append("${stack.size}\n")
-            "4" -> {
-                if (stack.empty()) {
-                    sb.append("${1}\n")
-                } else {
-                    sb.append("${0}\n")
-                }
-            }
-            "5" -> {
-                if (stack.empty()) {
-                    sb.append("${-1}\n")
-                } else {
-                    sb.append("${stack.peek()}\n")
-                }
-            }
+            "4" -> sb.append(if(stack.empty()) "${1}\n" else "${0}\n")
+            "5" -> sb.append(if(stack.empty()) "${-1}\n" else "${stack.peek()}\n")
         }
     }
     bw.write(sb.toString())
     bw.close()
-    br.close()
 }
