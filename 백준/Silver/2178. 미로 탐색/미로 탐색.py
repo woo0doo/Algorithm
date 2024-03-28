446b5cff7461e2c20ccd60aa1df7de5707c1b650
우영두 <duden5000@gmail.com>
2024-03-28 11:27:59 +0900

    [Silver I] Title: 미로 탐색, Time: 76 ms, Memory: 34184 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.py
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.py
@@ -1,31 +1,20 @@
 from collections import deque
 
+N,M = map(int, input().split())
+maze = [list(map(int,input())) for _ in range(N)]
 
-
-def bfs(x,y):
-    queue = deque()
-    queue.append((x,y))
-    while queue:
-        x,y=queue.popleft()
-        for i in range(4):
-            nx = x+dx[i]
-            ny = y+dy[i]
-            if nx < 0 or nx >= n or ny < 0 or ny >= m:
-                continue
-            if graph[x][y] == 0:
-                continue
-            if graph[nx][ny] == 1:
-                graph[nx][ny] = graph[x][y] + 1
-                queue.append((nx,ny))
-
-    return graph[n-1][m-1]
-
-n,m = map(int,input().split())
-
-graph=[]
-for i in range(n):
-    graph.append(list(map(int,input())))
-dx=[-1,1,0,0]
-dy=[0,0,-1,1]
-
-print(bfs(0,0))
+d=[(-1,0),(1,0),(0,-1),(0,1)]
+q = deque()
+q.append((0,0))
+def bfs():
+    while q:
+        x, y = q.popleft()
+        for k in range(len(d)):
+            dx = x + d[k][0]
+            dy = y + d[k][1]
+            if 0 <= dx < N and 0 <= dy < M:
+                if maze[dx][dy] == 1:
+                    maze[dx][dy] = maze[x][y] + 1
+                    q.append((dx, dy))
+bfs()
+print(maze[N-1][M-1])
