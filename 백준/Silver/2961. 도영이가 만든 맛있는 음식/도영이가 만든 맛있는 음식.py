69d329a078f1357f0b74a1972c91c6fc452c0504
우영두 <duden5000@gmail.com>
2024-04-07 10:59:31 +0900

    [Silver II] Title: 도영이가 만든 맛있는 음식, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
+++ 백준/Silver/2961. 도영이가 만든 맛있는 음식/도영이가 만든 맛있는 음식.py
@@ -1,25 +1,16 @@
-def dfs(depth, cnt, lemon, bad):
-    global result
 
-    # 가지 치기
+def backtracking(S, B, idx, cnt):
+    global answer
     if cnt != 0:
-        if abs(lemon - bad) < result:
-            result = abs(lemon - bad)
-
-    # 기저
-    if depth == N:
+        if abs(S - B) < answer:
+            answer = abs(S - B)
+    if idx >= N:
         return
-
-    # 재귀
-    dfs(depth + 1, cnt + 1, lemon * cuisine[depth][0], bad + cuisine[depth][1])  # 깊이와 개수 모두 증가시켜준다.
-    dfs(depth + 1, cnt, lemon, bad)  # 깊이만 증가시키고 개수는 증가시키지 않는다.
-
+    backtracking(S * foods[idx][0], B + foods[idx][1], idx+1, cnt+1)
+    backtracking(S, B, idx+1, cnt)
 
 N = int(input())
-cuisine = [list(map(int, input().split())) for i in range(N)]
-arr = []
-result = 1 << 100  # 결과값을 처음에 아주 큰 값으로 초기화
-
-dfs(0, 0, 1, 0)
-
-print(result)
+answer = float('inf')
+foods = [list(map(int, input().split())) for i in range(N)]
+backtracking(1, 0, 0, 0)
+print(answer)
